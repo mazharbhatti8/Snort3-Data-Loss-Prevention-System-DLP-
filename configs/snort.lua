f5e4b669c4a2af55af8441d9f3e1c4faefa0c95a
mazharbhatti8 <mazhar.69516@iqra.edu.pk>
2026-07-01 20:15:01 +0500

    Enhance snort.lua with configuration details
    Added comments to explain the Snort configuration file.

--- configs/snort.lua
+++ configs/snort.lua
@@ -1,1 +1,15 @@
+# Configuration Files
 
+This folder contains the Snort 3 configuration used in this project.
+
+Main configuration file:
+
+- snort.lua
+
+The configuration loads the custom rules from:
+
+/usr/local/etc/snort/rules/local.rules
+
+use sudo nano to paste rules in this file.
+
+The AFPacket module was used to capture live network traffic from the monitored interface.
